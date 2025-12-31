ac9742a8d1734e591172ef0d13ca8ab1bd2e31d7
Jeremy Brubaker <jbru362@gmail.com>
2025-12-30 20:27:00 -0500

    Refactor main() and others to:
    - Properly handle Options and Results
    - Simplify command line processing to remove duplicate code

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ enum Commands {
 //
 /// Convert a Springsharp 3b3 file to sharpie format and show the ship report.
 ///
-fn convert_ship(binding: MainWindow) {
+fn convert_ship(ui: MainWindow) {
     let file = FileDialog::new()
         .set_title("Springsharp file to convert")
         .add_filter(SS_SHIP_FILE_EXT, &[SS_SHIP_FILE_EXT,])
@@ -56,11 +56,9 @@ fn convert_ship(binding: MainWindow) {
         .into_string()
         .unwrap();
 
-    let ship = Ship::convert(file);
-
-    match ship {
+    match Ship::convert(file) {
         Ok(ship) => {
-            binding.set_report_str(ship.report().into());
+            ui.set_report_str(ship.report().into());
             save_ship(ship);
         },
 
@@ -71,7 +69,7 @@ fn convert_ship(binding: MainWindow) {
 
 /// Load a sharpie ship file and show the ship report.
 ///
-fn load_ship(binding: MainWindow) {
+fn load_ship(ui: MainWindow) {
     let file = FileDialog::new()
         .set_title("Sharpie file to load")
         .add_filter(SHIP_FILE_EXT, &[SHIP_FILE_EXT,])
@@ -82,10 +80,10 @@ fn load_ship(binding: MainWindow) {
         .into_string()
         .unwrap();
 
-    let ship = Ship::load(file);
+    match Ship::load(file) {
+        Ok(ship) =>
+            ui.set_report_str(ship.report().into()),
 
-    match ship {
-        Ok(ship) => binding.set_report_str(ship.report().into()),
         // TODO: Show errors in the GUI
         Err(error) => eprintln!("{}", error),
     };
@@ -105,80 +103,75 @@ fn save_ship(ship: Ship) {
         .into_string()
         .unwrap();
 
-    let _ = ship.save(file);
+    match ship.save(file) {
+        Ok(_) => (),
+        // TODO: Show errors in the GUI
+        Err(error) => eprintln!("{}", error),
+    };
 }
 
-slint::include_modules!();
+// Error handling {{{1
+//
+fn die(error: Box<dyn Error>) -> Result<(), Box<dyn Error>> {
+    eprintln!("{}", error);
+    Err(error)
+}
+
+// Run the GUI {{{1
+//
+fn run_gui() -> Result<(), Box<dyn Error>> {
+    let ui = MainWindow::new().unwrap();
+
+    ui.on_load_ship   ({ let h = ui.as_weak(); move || { load_ship(h.unwrap()); }});
+    ui.on_convert_ship({ let h = ui.as_weak(); move || { convert_ship(h.unwrap()); }});
+
+    match ui.run() {
+        Ok(_) => Ok(()),
+        Err(err) => Err(err.into()),
+    }
+}
 
 // Main {{{1
 //
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
 
-    let mut report_txt = None;
-    let mut internals = None;
-
-    let result = match cli.command {
+     match cli.command {
         Some(Commands::Load { file }) => {
             match Ship::load(file) {
                 Ok(ship) => {
-                    report_txt = Some(ship.report());
-                    if cli.debug {
-                        internals = Some(ship.internals());
-                    }
+                    println!("{}", ship.report());
+                    if cli.debug { eprintln!("{}", ship.internals()); }
+
                     Ok(())
                 },
-                Err(err) => Err(err),
+
+                Err(error) => die(error),
             }
         },
 
         Some(Commands::Convert { from, to, report }) => {
             match Ship::convert(from) {
                 Ok(ship) => {
-                    if report {
-                        report_txt = Some(ship.report());
-                    }
-                    if cli.debug {
-                        internals = Some(ship.internals());
-                    }
+                    if report    { println!("{}", ship.report()); }
+                    if cli.debug { eprintln!("{}", ship.internals()); }
+
                     match to {
-                        Some(to) => ship.save(to),
+                        Some(to) => match ship.save(to) {
+                            Ok(_) => Ok(()),
+                            Err(error) => die(error),
+                        },
+
                         None => Ok(()),
                     }
                 },
-                Err(err) => Err(err),
+
+                Err(error) => die(error),
             }
         },
 
-        None => {
-            let ui = MainWindow::new().unwrap();
-
-            ui.on_load_ship({ let handle = ui.as_weak(); move || { load_ship(handle.unwrap()); }});
-            ui.on_convert_ship({ let handle = ui.as_weak(); move || { convert_ship(handle.unwrap()); }});
-
-            let _ = ui.run();
-
-            Ok(())
-        }
-    };
-
-    match report_txt {
-        Some(txt) => println!("{}", txt),
-        None => (),
-    }
-
-    match internals {
-        Some(txt) => eprintln!("{}", txt),
-        None => (),
-    }
-
-    match result {
-        Ok(_) => Ok(()),
-        Err(error) => {
-            println!("{}", error);
-
-            Err(error)
-        },
+        // No subcommand means launch the GUI
+        None => run_gui(),
     }
 }
 
