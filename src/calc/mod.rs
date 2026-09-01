e5824ec705c1322cae1f802566e62369f2c2aae8
Jeremy Brubaker <jbrubake@orionarts.io>
2026-09-01 13:51:00 -0400

    Move "global" stuff into calc module

--- src/calc/mod.rs
+++ src/calc/mod.rs
@@ -12,8 +12,15 @@ pub mod units;
 pub mod weights;
 pub mod macros;
 
-#[cfg(test)]
-pub(crate) use ship::test_support;
+/// Earliest valid year for a ship.
+pub const YEAR_MIN: u32 = 1850;
+/// Latest valid year for a ship.
+pub const YEAR_MAX: u32 = 1950;
+
+/// File extension for sharpie files.
+pub const SHIP_FILE_EXT: &str = "ship";
+/// File extension for SpringSharp files.
+pub const SS_SHIP_FILE_EXT: &str = "sship";
 
 pub use armor::*;
 pub use asw::*;
@@ -26,3 +33,64 @@ pub use ship::*;
 pub use torpedoes::*;
 pub use units::*;
 pub use weights::*;
+
+// Testing support {{{1
+#[cfg(test)]
+pub(crate) mod test_support {
+    use crate::calc::Ship;
+    use crate::calc::Hull;
+    use crate::calc::Engine;
+    use crate::calc::engine::{BoilerType, DriveType, FuelType};
+    use crate::calc::units::{Measurement, Units, UnitType::*};
+
+    // Round a float to a given number of digits
+    //
+    // This makes it much easier to test results that
+    // are floats.
+    pub fn to_place(n: f64, digits: u32) -> f64 {
+        let mult = 10_u32.pow(digits) as f64;
+
+        (n * mult).round() / mult
+    }
+
+    pub fn test_ship() -> Ship {
+        let mut hull = Hull::default();
+
+        hull.set_lwl(100.0, Units::Imperial);
+        hull.set_d(1000.0);
+
+        hull.b  = Measurement::new(50.0, LengthLong, Units::Imperial);
+        hull.bb = Measurement::new(hull.b.imp(), LengthLong, Units::Imperial);
+        hull.t  = Measurement::new(10.0, LengthLong, Units::Imperial);
+
+        hull.freeboard.fc_len = 0.2;
+        hull.freeboard.fc_fwd = Measurement::new(10.0, LengthLong, Units::Imperial);
+        hull.freeboard.fc_aft = hull.freeboard.fc_fwd;
+
+        hull.freeboard.fd_len = 0.3;
+        hull.freeboard.fd_fwd = hull.freeboard.fc_fwd;
+        hull.freeboard.fd_aft = hull.freeboard.fc_fwd;
+
+        hull.freeboard.ad_fwd = hull.freeboard.fc_fwd;
+        hull.freeboard.ad_aft = hull.freeboard.fc_fwd;
+
+        hull.freeboard.qd_len = 0.15;
+        hull.freeboard.qd_fwd = hull.freeboard.fc_fwd;
+        hull.freeboard.qd_aft = hull.freeboard.fc_fwd;
+
+        let mut engine = Engine::default();
+        engine.set_shafts(2, &mut hull);
+        engine.year     = 1920;
+        engine.fuel     = FuelType::Oil;
+        engine.boiler   = BoilerType::Turbine;
+        engine.drive    = DriveType::Geared;
+        engine.vmax     = 30.0;
+        engine.vcruise  = 20.0;
+        engine.range    = 10000;
+
+        let mut ship = Ship::default();
+        ship.hull  = hull;
+        ship.engine = engine;
+        ship
+    }
+}
--- src/calc/ship.rs
+++ src/calc/ship.rs
@@ -18,6 +18,8 @@ use crate::calc::{
     Torpedoes,
     UnitType::*,
     Units::*,
+    YEAR_MIN,
+    YEAR_MAX,
 };
 use crate::{addto, addif, num, pct};
 
@@ -29,16 +31,6 @@ use std::error::Error;
 use std::fs::{self, File, OpenOptions};
 use std::io::{BufRead, BufReader, Write};
 
-/// Earliest valid year for a ship.
-pub const YEAR_MIN: u32 = 1850;
-/// Latest valid year for a ship.
-pub const YEAR_MAX: u32 = 1950;
-
-/// File extension for sharpie files.
-pub const SHIP_FILE_EXT: &str = "ship";
-/// File extension for SpringSharp files.
-pub const SS_SHIP_FILE_EXT: &str = "sship";
-
 /// The Ship file version created by this version of sharpie.
 pub const SHIP_FILE_VERSION: u32 = 1;
 
@@ -48,68 +40,6 @@ pub const SHIP_FILE_VERSION: u32 = 1;
 #[derive(Serialize, Deserialize, Clone, Debug)]
 struct Version {
     version: u32,
-}
-
-// Testing support {{{1
-#[cfg(test)]
-pub(crate) mod test_support {
-    use crate::calc::Ship;
-    use crate::calc::Hull;
-    use crate::calc::Engine;
-    use crate::calc::engine::{BoilerType, DriveType, FuelType};
-    use crate::calc::units::{Measurement, Units, UnitType::*};
-
-    // Round a float to a given number of digits
-    //
-    // This makes it much easier to test results that
-    // are floats.
-    pub fn to_place(n: f64, digits: u32) -> f64 {
-        let mult = 10_u32.pow(digits) as f64;
-
-        (n * mult).round() / mult
-    }
-
-    pub fn test_ship() -> Ship {
-        let mut hull = Hull::default();
-
-        hull.set_lwl(100.0, Units::Imperial);
-        hull.set_d(1000.0);
-
-        hull.b  = Measurement::new(50.0, LengthLong, Units::Imperial);
-        hull.bb = Measurement::new(hull.b.imp(), LengthLong, Units::Imperial);
-        hull.t  = Measurement::new(10.0, LengthLong, Units::Imperial);
-
-        hull.freeboard.fc_len = 0.2;
-        hull.freeboard.fc_fwd = Measurement::new(10.0, LengthLong, Units::Imperial);
-        hull.freeboard.fc_aft = hull.freeboard.fc_fwd;
-
-        hull.freeboard.fd_len = 0.3;
-        hull.freeboard.fd_fwd = hull.freeboard.fc_fwd;
-        hull.freeboard.fd_aft = hull.freeboard.fc_fwd;
-
-        hull.freeboard.ad_fwd = hull.freeboard.fc_fwd;
-        hull.freeboard.ad_aft = hull.freeboard.fc_fwd;
-
-        hull.freeboard.qd_len = 0.15;
-        hull.freeboard.qd_fwd = hull.freeboard.fc_fwd;
-        hull.freeboard.qd_aft = hull.freeboard.fc_fwd;
-
-        let mut engine = Engine::default();
-        engine.set_shafts(2, &mut hull);
-        engine.year     = 1920;
-        engine.fuel     = FuelType::Oil;
-        engine.boiler   = BoilerType::Turbine;
-        engine.drive    = DriveType::Geared;
-        engine.vmax     = 30.0;
-        engine.vcruise  = 20.0;
-        engine.range    = 10000;
-
-        Ship {
-            hull,
-            engine,
-            ..Default::default()
-        }
-    }
 }
 
 // Ship {{{1
